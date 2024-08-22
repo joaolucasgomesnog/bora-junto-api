70455a052166e4d9cd6cd975a9e17686f7cc74e8
Fabricyo <fabricyosilva600@gmail.com>
2024-08-22 11:32:25 -0300

    hotfix on the user

--- src/modules/User/index.js
+++ src/modules/User/index.js
@@ -20,7 +20,7 @@ export default {
       user_category_id,
       profile_pic_url,
       phone,
-      email,
+      email
     );
     console.log("usuario", username);
 
@@ -35,7 +35,6 @@ export default {
           user_category_id: parseInt(user_category_id),
           profile_pic_url,
           location_id,
-
         },
       });
 
@@ -47,8 +46,8 @@ export default {
   },
 
   async getContactListByUserId(req, res) {
-    const {user_id} = req.params;
-    console.log("ID", user_id)
+    const { user_id } = req.params;
+    console.log("ID", user_id);
     try {
       const user = await prisma.user.findUnique({
         where: {
@@ -56,39 +55,39 @@ export default {
         },
         include: {
           contact: true,
-          user_category:true
-           
+          user_category: true,
         },
       });
-  
+
       if (!user) {
-        
         console.error("user not found");
         return res.status(404).json({ error: "User not found" });
       }
-      
+
       res.json(user.contact);
     } catch (error) {
       console.error("Error while getting contacts:", error);
       res.status(500).json({ error: "Error while getting contacts" });
     }
   },
-  
-  async setContactToUser(req, res){
+
+  async setContactToUser(req, res) {
     const { user_id } = req.params;
     const { contact_id } = req.body;
 
-    console.log("UPDATE", contact_id, user_id)
-  
+    console.log("UPDATE", contact_id, user_id);
+
     try {
       // Verificar se o usuário e o contato existem
       const user = await prisma.user.findUnique({ where: { id: user_id } });
-      const contact = await prisma.user.findUnique({ where: { id: contact_id } });
-  
+      const contact = await prisma.user.findUnique({
+        where: { id: contact_id },
+      });
+
       if (!user || !contact) {
         return res.status(404).json({ error: "User or contact not found" });
       }
-  
+
       // Adicionar o contato à lista de contatos do usuário
       const updatedUser = await prisma.user.update({
         where: { id: user_id },
@@ -98,16 +97,16 @@ export default {
           },
         },
         include: {
-          contact: true,// Inclui a lista atualizada de contatos na resposta
-          user_category:true          },
+          contact: true, // Inclui a lista atualizada de contatos na resposta
+          user_category: true,
+        },
       });
-  
+
       res.json(updatedUser.contacts);
     } catch (error) {
       console.error("Error while adding contact:", error);
       res.status(500).json({ error: "Error while adding contact" });
     }
-
   },
 
   async findAllUsers(req, res) {
@@ -119,34 +118,39 @@ export default {
     }
   },
 
-  async filterAllUsers(req, res){
-    const {query} = req.query
-    console.log("QUERY", query)
+  async filterAllUsers(req, res) {
+    const { query } = req.query;
+    console.log("QUERY", query);
     try {
       const users = await prisma.user.findMany({
         where: {
           username: {
             contains: query,
             mode: "insensitive",
-          }
-          
+          },
         },
         include: {
-          user_category:true
-        }
-      })
-      console.log("USER FILTERED", users)
+          user_category: true,
+        },
+      });
+      console.log("USER FILTERED", users);
       return res.json(users);
     } catch (error) {
-      return res.status(500).json({ error: error})
+      return res.status(500).json({ error: error });
     }
   },
 
   async findUserById(req, res) {
     try {
       const { id } = req.params;
-      const user = await prisma.user.findUnique({ where: { id }, include: {user_category: true}});
-      if (!user) return res.json({ error: "User does not exist" });
+      const user = await prisma.user.findUnique({
+        where: { id },
+        include: { user_category: true },
+      });
+      if (!user) {
+        console.log("nao encontrado")
+        return res.json({ error: "User does not exist" });
+      }
       return res.json(user);
     } catch (error) {
       return res.json({ error });
@@ -185,6 +189,7 @@ export default {
         phone,
         email,
         birth_date,
+        description,
       } = req.body;
 
       let user = await prisma.user.findUnique({ where: { id } });
@@ -198,6 +203,7 @@ export default {
           email,
           phone,
           birth_date,
+          description,
         },
       });
       return res.json(user);
