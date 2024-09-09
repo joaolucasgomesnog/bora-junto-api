795d206e7768c22a34461eedd8b2da3474d11831
joaolucasgomesnog <lucasgomes145987@gmail.com>
2024-09-09 10:27:45 -0300

    .

--- src/modules/ChallengeParticipant/index.js
+++ src/modules/ChallengeParticipant/index.js
@@ -1,13 +1,13 @@
 import { prisma } from "../../lib/prisma.js";
 
 export default {
-  
-  async createParticipant(req, res){
-    const {user_id, challenge_id} = req.body
-    const challenge_exists = await prisma.challenge.findUnique({where: { id: Number(challenge_id) }});
-    const user_exists = await prisma.user.findUnique({where: { id: user_id }});
-    
-    if(!challenge_exists && !user_exists){
+
+  async createParticipant(req, res) {
+    const { user_id, challenge_id } = req.body
+    const challenge_exists = await prisma.challenge.findUnique({ where: { id: Number(challenge_id) } });
+    const user_exists = await prisma.user.findUnique({ where: { id: user_id } });
+
+    if (!challenge_exists && !user_exists) {
       res.status(500).json({ error: "Event or User not founds" });
       console.log("Event or User not founds")
       return
@@ -22,7 +22,7 @@ export default {
       }
     });
 
-    if(participant_exists){
+    if (participant_exists) {
       res.status(500).json({ error: "Participant already exists" });
       console.log("Participant already exists")
       return;
@@ -39,7 +39,7 @@ export default {
     } catch (error) {
       console.error("Erro while creating participant", error);
       res.status(500).json({ error: "Erro while creating a new participant" });
-      
+
     }
   },
 
@@ -47,24 +47,24 @@ export default {
   //fazer umas função so para verificar se existe
   async getParticipantById(req, res) {
     const { user_id, challenge_id } = req.body;
-  
+
     console.log("TESTE", user_id, challenge_id);
     try {
       const user_exists = await prisma.user.findUnique({
         where: { id: user_id },
       });
-  
+
       const challenge_exists = await prisma.challenge.findUnique({
         where: { id: Number(challenge_id) },
       });
-      
+
       if (!challenge_exists || !user_exists) {
         res.status(404).json({ error: "Event or User not found" });
         console.log("Event or User not found");
         return;
       }
-  
-  
+
+
       const participant_exists = await prisma.challengeParticipant.findUnique({
         where: {
           challenge_id_user_id: {
@@ -73,13 +73,13 @@ export default {
           },
         },
       });
-  
+
       if (participant_exists) {
         console.log("Participant already exists");
         res.json({ participant_already_exists: true });
         return;
       }
-  
+
       res.json({ participant_already_exists: false });
     } catch (error) {
       console.error("Error while checking participant", error);
@@ -93,84 +93,101 @@ export default {
     try {
 
       const challenge_exists = await prisma.challenge.findUnique({
-        where: { id: Number(challenge_id) },
+        where: { id: Number(challenge_id) }
       });
-      
+
       if (!challenge_exists) {
         res.status(404).json({ error: "Challenge not found" });
         console.log("Challenge not found");
         return;
       }
-  
-  
+
+
+      // Busca os participantes do challenge
       const participants = await prisma.challengeParticipant.findMany({
         where: {
-          challenge_id: challengeId
+          challenge_id: challengeId,
         },
-        include:{
-          user:{
-            select:{
-              name:true,
-              username:true,
-              profile_pic_url:true
-            }
-          }
-        }
-      });
-  
-      if (participants) {
-        res.json(participants);
-        return;
-      }
-  
-      res.json('erro');
-    } catch (error) {
-      console.error("Error while checking participants", error);
-      res.status(500).json({ error: "Error while checking participants" });
-    }
-  },
-
-  async deleteParticipantById(req, res) {
-    const { user_id, challenge_id } = req.body;
-  
-    console.log("TESTE", user_id, challenge_id);
-    try {
-      const user_exists = await prisma.user.findUnique({
-        where: { id: user_id },
-      });
-  
-      const challenge_exists = await prisma.challenge.findUnique({
-        where: { id: Number(challenge_id) },
-      });
-      
-      if (!challenge_exists || !user_exists) {
-        res.status(404).json({ error: "Event or User not found" });
-        console.log("Event or User not found");
-        return;
-      }
-  
-  
-      const participant_deleted = await prisma.challengeParticipant.delete({
-        where: {
-          challenge_id_user_id: {
-            challenge_id: Number(challenge_id),
-            user_id: user_id,
+        include: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              username: true,
+              profile_pic_url: true,
+            },
           },
         },
       });
-  
-      if (participant_deleted) {
-        console.log("Participant deleted");
-        res.json({ participant_deleted: true });
-        return;
-      }
-  
-      res.json({ participant_deleted: false });
+
+      // Itera sobre os participantes e busca a contagem de check-ins para cada um
+      const participantsWithCheckIns = await Promise.all(
+        participants.map(async (participant) => {
+          const checkInCount = await prisma.checkIn.count({
+            where: {
+              challenge_id: challengeId,
+              user_id: participant.user_id,
+            },
+          });
+
+          return {
+            ...participant,
+            checkInCount, // Adiciona a contagem de check-ins
+          };
+        })
+      );
+
+      // Retorna o resultado como JSON
+      return res.json(participantsWithCheckIns);
+
     } catch (error) {
-      console.error("Error while deleting participant", error);
-      res.status(500).json({ error: "Error while deleting participant" });
+      console.error(error);
+      return res.status(500).json({ error: "Erro ao buscar participantes e check-ins." });
     }
-  }
+  },
   
-};
+
+  async deleteParticipantById(req, res) {
+      const { user_id, challenge_id } = req.body;
+
+      console.log("TESTE", user_id, challenge_id);
+      try {
+        const user_exists = await prisma.user.findUnique({
+          where: { id: user_id },
+        });
+
+        const challenge_exists = await prisma.challenge.findUnique({
+          where: { id: Number(challenge_id) },
+        });
+
+        if (!challenge_exists || !user_exists) {
+          res.status(404).json({ error: "Event or User not found" });
+          console.log("Event or User not found");
+          return;
+        }
+
+
+        const participant_deleted = await prisma.challengeParticipant.delete({
+          where: {
+            challenge_id_user_id: {
+              challenge_id: Number(challenge_id),
+              user_id: user_id,
+            },
+          },
+        });
+
+        if (participant_deleted) {
+          console.log("Participant deleted");
+          res.json({ participant_deleted: true });
+          return;
+        }
+
+        res.json({ participant_deleted: false });
+      } catch (error) {
+        console.error("Error while deleting participant", error);
+        res.status(500).json({ error: "Error while deleting participant" });
+      }
+    }
+
+  };
 
