337166b872294d665835dafc10f9ee6db20d5fc7
lucasgomesnog <lucasgomes145987@gmail.com>
2024-10-08 14:33:40 -0300

    post stats

--- src/modules/Post/index.js
+++ src/modules/Post/index.js
@@ -59,15 +59,46 @@ export default {
 
 
     },
-    async getAllPosts(req, res){
+    async getAllPosts(req, res) {
+        const { user_id } = req.params; // Supondo que o ID do usuário vem dos parâmetros da requisição
+    
         try {
-            const all_posts = await prisma.post.findMany({include: {User: {select: {username:true, profile_pic_url:true,name:true}}}})
-            res.json(all_posts)
+            const all_posts = await prisma.post.findMany({
+                include: {
+                    User: {
+                        select: {
+                            username: true,
+                            profile_pic_url: true,
+                            name: true,
+                        },
+                    },
+                    like: {
+                        where: {
+                            user_id: user_id,
+                        },
+                        select: {
+                            id: true, // Pode incluir mais campos, se necessário
+                        },
+                    },
+                    comment: {
+                        where: {
+                            user_id: user_id,
+                        },
+                        select: {
+                            id: true, // Pode incluir mais campos, se necessário
+                        },
+                    },
+                },
+            });
+
+    
+            res.json(all_posts);
         } catch (error) {
-            console.error('Erro while creating post', error)
-            res.status(500).json({erro: 'Erro while updating post', error})
+            console.error('Erro ao buscar posts:', error);
+            res.status(500).json({ erro: 'Erro ao buscar posts', error });
         }
     },
+    
     async getAllPostsById(req, res){
         const {id} = req.params
         const post_id = parseInt(id)
@@ -83,6 +114,130 @@ export default {
             res.status(500).json({erro: 'Erro while updating post', error})
         }
     },
+    async likePost(req, res) {
+        const { user_id, post_id } = req.params;
+    
+        try {
+            // Cria o like
+            const like = await prisma.like.create({
+                data: {
+                    post_id: post_id,
+                    user_id: user_id,
+                },
+            });
+    
+            if (like) {
+                // Busca o post atual para obter o valor de count_likes
+                const post = await prisma.post.findUnique({
+                    where: {
+                        id: post_id,
+                    },
+                    select: {
+                        count_likes: true, // Busca apenas o campo count_likes
+                    },
+                });
+    
+                // Atualiza o campo count_likes
+                const updated_post = await prisma.post.update({
+                    where: {
+                        id: post_id,
+                    },
+                    data: {
+                        count_likes: post.count_likes + 1, // Incrementa o valor atual de count_likes
+                    },
+                });
+    
+                res.json(updated_post);
+            }
+        } catch (error) {
+            console.error('Erro ao criar like e atualizar o post:', error);
+            res.status(500).json({ erro: 'Erro ao atualizar o post', error });
+        }
+    },
+
+    async commentPost(req, res) {
+        const { user_id, post_id } = req.params;
+        const { description } = req.body;
+        try {
+            // Cria o comment
+            const comment = await prisma.comment.create({
+                data: {
+                    post_id: post_id,
+                    user_id: user_id,
+                    description,
+                },
+            });
+    
+            if (comment) {
+                // Busca o post atual para obter o valor de count_comments
+                const post = await prisma.post.findUnique({
+                    where: {
+                        id: post_id,
+                    },
+                    select: {
+                        count_comments: true, // Busca apenas o campo count_comments
+                    },
+                });
+    
+                // Atualiza o campo count_comments
+                const updated_post = await prisma.post.update({
+                    where: {
+                        id: post_id,
+                    },
+                    data: {
+                        count_comments: post.count_comments + 1, // Incrementa o valor atual de count_comments
+                    },
+                });
+    
+                res.json(updated_post);
+            }
+        } catch (error) {
+            console.error('Erro ao criar comment e atualizar o post:', error);
+            res.status(500).json({ erro: 'Erro ao atualizar o post', error });
+        }
+    },
+    
+    async sharePost(req, res) {
+        const { user_id, post_id } = req.params;
+        try {
+            // Cria o comment
+            const share = await prisma.share.create({
+                data: {
+                    post_id: post_id,
+                    user_id: user_id,
+                },
+            });
+    
+            if (share) {
+                // Busca o post atual para obter o valor de count_shares
+                const post = await prisma.post.findUnique({
+                    where: {
+                        id: post_id,
+                    },
+                    select: {
+                        count_shares: true, // Busca apenas o campo count_shares
+                    },
+                });
+    
+                // Atualiza o campo count_shares
+                const updated_post = await prisma.post.update({
+                    where: {
+                        id: post_id,
+                    },
+                    data: {
+                        count_shares: post.count_shares + 1, // Incrementa o valor atual de count_comments
+                    },
+                });
+    
+                res.json(updated_post);
+            }
+        } catch (error) {
+            console.error('Erro ao arquivar compartilhamento e atualizar o post:', error);
+            res.status(500).json({ erro: 'Erro ao atualizar o post', error });
+        }
+    }
+
+    
     // async getCommentsByPost(req, res) {
     //     try {
     //         const { postId } = req.params
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,7 +6,9 @@ const postRoutes = Router()
 postRoutes.post('/', post.createPost)
 postRoutes.delete('/:id', post.deletePost)
 postRoutes.put('/:id', post.updatePost)
-postRoutes.get('/list', post.getAllPosts)
+postRoutes.get('/:user_id/feed', post.getAllPosts)
 postRoutes.get('/list/:id', post.getAllPostsById)
+postRoutes.put('/:post_id/:user_id/like', post.likePost)
+postRoutes.put('/:post_id/:user_id/comment', post.commentPost)
 
 export { postRoutes }
