5ed71103cff581fcf9203aa30e738bf4d1e600b4
Fabricyo <fabricyosilva600@gmail.com>
2024-10-16 22:17:17 -0300

    feed push notifications working

--- src/modules/Notification/index.js
+++ src/modules/Notification/index.js
@@ -1,5 +1,5 @@
 import { prisma } from "../../lib/prisma.js";
-import PushNotification from '../PushNotification/index.js'
+import Notification from "../PushNotification/index.js";
 
 // Função para criar uma notificação
 
@@ -14,26 +14,53 @@ export default {
           postId, // postId é opcional, apenas para curtidas/comentários
         },
       });
+
+      const {username: sender_name} = await prisma.user.findUnique({
+        where: { id: triggeredById },
+        select: {username: true}
+      })
+
+      let notificationMessage = `${sender_name} voçê tem novas atualizações.`;
+
+      if(newNotification.type === 'like'){
+        notificationMessage = `${sender_name} curtiu sua publicação.`
+      }else if (newNotification.type === 'comment'){
+        notificationMessage = `${sender_name} comentou a sua publicação.`
+      }else if(following_id){
+        notificationMessage = `${sender_name} começou a te seguir.`
+      }
+
+      const {notificationTokens} = await Notification.getNotificationTokenByUserId(userId)
+
+      if (notificationTokens.length > 0) {
+        await Promise.all(
+          notificationTokens.map(async (token) => {
+            await Notification.sendPushNotification(token, notificationMessage);
+          })
+        );
+        console.log('Notificações enviadas para todos os tokens');
+      }
+
       return newNotification;
     } catch (error) {
-      console.error('Erro ao criar notificação:', error);
+      console.error("Erro ao criar notificação:", error);
       throw error;
     }
   },
-  
+
   // Função para obter todas as notificações por userId
   async getAllNotificationsByUserId(req, res) {
     try {
-      const {id} = req.params
+      const { id } = req.params;
 
       const notifications = await prisma.notification.findMany({
         where: { userId: id },
         include: {
           Post: {
             select: {
-              id: true,          // Selecionar apenas o ID do post
+              id: true, // Selecionar apenas o ID do post
               description: true, // Selecionar a descrição do post
-              media_url: true,   // Selecionar a URL da mídia (se aplicável)
+              media_url: true, // Selecionar a URL da mídia (se aplicável)
             },
           },
           triggeredBy: {
@@ -44,30 +71,28 @@ export default {
             },
           },
         },
-        orderBy: { created_at: 'desc' }, // Ordenar pela mais recente
+        orderBy: { created_at: "desc" }, // Ordenar pela mais recente
       });
-      
+
       return res.status(200).json(notifications);
     } catch (error) {
-      console.error('Erro ao obter notificações:', error);
+      console.error("Erro ao obter notificações:", error);
       throw error;
     }
   },
-  
+
   // Função para deletar todas as notificações por userId
   async deleteAllNotificationsByUserId(req, res) {
     try {
-      const {id} = req.params
+      const { id } = req.params;
 
       const deleteCount = await prisma.notification.deleteMany({
         where: { userId: id },
       });
       return deleteCount;
     } catch (error) {
-      console.error('Erro ao deletar notificações:', error);
+      console.error("Erro ao deletar notificações:", error);
       throw error;
     }
-  }
-
-}
-
+  },
+};
